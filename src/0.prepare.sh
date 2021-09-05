338ad7f9c09bfb0aba87b2895d4554a8d59ab739
Septimiu Fabian Mare <septimiu.mare@tii.ae>
2021-09-05 11:56:53 +0400

    - Added script to prepare SAL provider, build it and pull required files, readying the development for direct building using `cmake .` from the source folder

---
+++ src/0.prepare.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir .temp
+cd .temp
+git clone https://github.com/miracl/core.git
+cd core/c
+python3 ./config
+python3 ./config64.py --options=2 --options=3 --options=8 --options=41 --options=43
+cd ../../../
+cp .temp/core/c/core.a vendor/miracl/
+cp .temp/core/c/core.h vendor/miracl/includes
+cp .temp/core/c/arch.h vendor/miracl/includes
+cp .temp/core/c/ecdh_*.h vendor/miracl/includes
+cp .temp/core/c/rsa*.h vendor/miracl/includes
+cp tls_sal_m.xpp tls_sal.cpp
+rm -rf .temp
