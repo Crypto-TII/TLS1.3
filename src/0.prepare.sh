b29c65d38dc4b1cfb8ddd5ece59a60de437aafb7
Septimiu Fabian Mare <septimiu.mare@tii.ae>
2021-09-06 12:17:12 +0400

    Correctly pull C++ version instead of the previous C version

--- src/0.prepare.sh
+++ src/0.prepare.sh
@@ -2,18 +2,10 @@
 mkdir .temp
 cd .temp
 git clone https://github.com/miracl/core.git
-cd core/c
-python3 ./config
+cd core/cpp
 python3 ./config64.py --options=2 --options=3 --options=8 --options=41 --options=43
 cd ../../../
-cp .temp/core/c/core.a vendor/miracl/
-cp .temp/core/c/core.h vendor/miracl/includes
-cp .temp/core/c/arch.h vendor/miracl/includes
-cp .temp/core/c/ecdh_*.h vendor/miracl/includes
-cp .temp/core/c/ecp_*.h vendor/miracl/includes
-cp .temp/core/c/fp_*.h vendor/miracl/includes
-cp .temp/core/c/big_*.h vendor/miracl/includes
-cp .temp/core/c/config_*.h vendor/miracl/includes
-cp .temp/core/c/rsa*.h vendor/miracl/includes
+cp .temp/core/cpp/core.a vendor/miracl/
+cp .temp/core/cpp/*.h vendor/miracl/includes
 cp tls_sal_m.xpp tls_sal.cpp
 rm -rf .temp
